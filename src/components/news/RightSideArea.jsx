710cdf76a65becb007d88a7ed606a12a8045668d
ahmedriadx10 <infinitecoderx02@gmail.com>
2026-04-27 22:57:15 +0600

    right side bar added

--- src/components/news/RightSideArea.jsx
+++ src/components/news/RightSideArea.jsx
@@ -1,5 +1,11 @@
 import { Button } from "@heroui/react";
 import {Icon} from "@iconify/react";
+import classShowImg from '@/assets/class.png'
+import playGroundImg from '@/assets/playground.png'
+import swimmingShow from '@/assets/swimming.png'
+import instagramImg from '@/assets/instagram.png'
+import Image from "next/image";
+import { FaFacebook, FaTwitter } from "react-icons/fa";
 const RightSideArea = () => {
   return (
     <div className=" col-span-2">
@@ -17,6 +23,40 @@ const RightSideArea = () => {
       </Button>
    
     </div>
+
+    <div>
+<h4 className="mt-7.5 font-semibold text-xl text-(--dark-2)">Find Us On</h4>
+
+<div className="border border-zinc-200 rounded-lg mt-5">
+
+<div className="flex items-center gap-2.5 p-4">
+<FaFacebook className="text-blue-500 text-3xl"/>
+<span className="text-(--dark-3) font-medium">Facebook</span>
+</div>
+<div className="flex items-center gap-2.5 p-4 border-y">
+<FaTwitter className="text-blue-500 text-3xl"/>
+<span className="text-(--dark-3) font-medium">Twitter</span>
+</div>
+<div className="flex items-center gap-2.5 p-4">
+  <Image src={instagramImg} alt="instagram image" width={30} height={30} />
+<span className="text-(--dark-3) font-medium">Instagram</span>
+</div>
+
+</div>
+    </div>
+
+
+    {/* Q Zone */}
+
+<div className="bg-[#F3F3F3] p-3.75 mt-5 space-y-5 rounded-lg">
+  <h5 className="text-xl font-semibold text(--dark-2)">Q-Zone</h5>
+
+<Image src={swimmingShow} alt="swimming image" height={219} width={258} ></Image>
+<Image src={classShowImg} alt="class room show" height={219} width={258} ></Image>
+<Image src={playGroundImg} alt="playground image" height={219} width={258} ></Image>
+
+</div>
+
     </div>
   );
 };
